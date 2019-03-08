e68d18fd4ca927ac83a698b42f6bc67a34ca5522
NoahAm <noah.andert06@gmx.at>
2019-03-08 13:11:58 +0100

    Cleaned the code up
    but only a tiny bit

--- cleaner.py
+++ cleaner.py
@@ -5,38 +5,51 @@ import matplotlib.pyplot as plt
 script, filename = argv
 data = {'date':[],'sender':[], 'msg':[]}
 
-def splitData(f):
-    text = ""
+def parseData(f):
+    chatData = []
+    #text = ""
     for line in f:
      try:
       splitted = line.split(': ')
       meta = splitted[0]
       msg = splitted[1]
       if('Medien' not in msg): # If any form of media is sent, WhatsApp prints "<Media Omitted>". Getting rid of this. TO DO: Improve this functionality.
-       text = text + "\n" + msg
-       data['msg'].append(msg)
-       splitMeta(meta)
+       #text = text + "\n" + msg
+       #data['msg'].append(msg)
+       #splitMeta(meta)
+       splittedMessage = {'msg':msg, 'date':0, 'sender':""}
+       splitMeta(meta,splittedMessage)
+       chatData.append(splittedMessage)
      except:
          pass
-    return text
+    return chatData
 
-def splitMeta(m):
+def splitMeta(m, d):
     try:
         splitted = m.split(" - ")
         date = splitted[0]
         sender = splitted[1]
-        data['date'].append(date)
-        data['sender'].append(sender)
+        #data['date'].append(date)
+        #data['sender'].append(sender)
+        splittedMessage['date'] = date
+        splittedMessage['sender'] = sender
     except:
-        print("Ups")
+        pass
+
+def getMsgTexts(chatData):
+    text = ""
+    for i in chatData:
+        text = text + "\n" + i['msg']
+
+    return text
 
 if __name__ == "__main__":
 
     f = open(filename,'r')
-    text = splitData(f)
-
-    for line in data['msg']:
-        print(line)
+    chat = parseData(f)
+    text = getMsgTexts(chat)
+    #for line in data['msg']:
+    #    print(line)
 
     wordcloud = WordCloud().generate(text)
 
